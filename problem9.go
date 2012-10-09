c556e3eecd373924d59d0ee78acda4fefb57e17d
Marcelo Moreira <marcelom@tsg-marcelom-dt.(none)>
2012-10-09 14:33:18 -0700

    problem 9 is OK, problem 8 is not done yet...

---
+++ problem9.go
@@ -0,0 +1,17 @@
+// Project Euler, Problem 9
+// Find the only Pythagorean triplet, {a, b, c}, for which a + b + c = 1000.
+
+package main
+
+import "fmt"
+
+func main() {
+	for a := 1; a < 998; a++ {
+		for b := 1; b < 999-a; b++ {
+			c = 1000 - a - b
+			if a*a+b*b == c*c {
+				fmt.Println("Found it: (a b c) = (", a, b, c, ")")
+			}
+		}
+	}
+}
