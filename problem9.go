c08d58f5b2f6e717665a4437838c3711f48b2c8c
Marcelo Moreira <marcelom@tsg-marcelom-dt.(none)>
2012-10-10 10:30:59 -0700

    fixed some issues in problem 9

--- problem9.go
+++ problem9.go
@@ -8,9 +8,10 @@ import "fmt"
 func main() {
 	for a := 1; a < 998; a++ {
 		for b := 1; b < 999-a; b++ {
-			c = 1000 - a - b
+			c := 1000 - a - b
 			if a*a+b*b == c*c {
 				fmt.Println("Found it: (a b c) = (", a, b, c, ")")
+				return
 			}
 		}
 	}
