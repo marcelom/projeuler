8198b3e9a3b030aa831db9190b67e5f545247602
Marcelo <marcelosm@gmail.com>
2012-12-19 14:18:28 -0800

    added a go solution to problem 14

---
+++ problem14.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	longest := uint32(0)
+	terms := uint32(0)
+
+	for i := uint32(1); i <= 1000000; i++ {
+		j := i
+		this_terms := uint32(1)
+
+		for j != 1 {
+			this_terms++
+
+			if this_terms > terms {
+				//fmt.Println(i,j)
+				terms = this_terms
+				longest = i
+			}
+
+			if j%2 == 0 {
+				j = j / 2
+			} else {
+				j = 3*j + 1
+			}
+
+		}
+	}
+
+	fmt.Println("Longest is", longest, "(with", terms, "terms)")
+}
